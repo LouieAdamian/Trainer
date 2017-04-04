61c4f9b4a96b402a1f7770c575bb8e3e91ed4b0f
Louie Adamian <LouieAdamian@users.noreply.github.com>
2017-04-04 11:32:54 -0400

    adds new webcam lib

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-  var NodeWebcam = require("node-webcam");
+var NodeWebcam = require("node-webcam");
 var fs = require('fs');
 var watson = require('watson-developer-cloud');
 var visual_recognition = watson.visual_recognition({
@@ -6,38 +6,38 @@ var visual_recognition = watson.visual_recognition({
     version: 'v3',
     version_date: '2016-05-20'
 });
-var pos_res;
+var Webcam = NodeWebcam.create( opts );
 var opts = {
-    width: [Number, 1920],
-    height: [Number, 1080],
-    delay: [Number, 0],
 
-    quality: [Number, 100],
+    width: [ Number, 640 ],
 
-    output: [String, "jpeg"],
+    height: [ Number, 480 ],
 
-    verbose: [Boolean, true],
+    delay: [ Number, 0 ],
 
-    help: [Boolean, false],
+    quality: [ Number, 100 ],
 
-    version: [Boolean, false],
+    output: [ String, "jpeg" ],
+
+    verbose: [ Boolean, true ],
+
+    help: [ Boolean, false ],
+
+    version: [ Boolean, false ],
+
     location: "./dog.jpg"
 
-}
-var Webcam = NodeWebcam.create(opts);
+};
 
+Webcam.capture( "dog.jpg", {}, function( err, data ) {
 
-NodeWebcam.capture("dog.jpg", {}, function(err, data) {
-
-    if (!err) {}
-    console.log("Image created!");
+    if ( !err ) console.log( "Image created!" );
     classify();
 });
-
-
+classify();
 function classify() {
     var img = {
-        images_file: fs.createReadStream('./dog.jpg'),
+        images_file: fs.createReadStream('./dog2.jpg'),
         classifier_ids: 'position_185473635'
     }
     visual_recognition.classify(img, function(err, res) {
@@ -45,12 +45,13 @@ function classify() {
             console.log(err);
         } else {
             pos_res = res;
-            //console.log(JSON.stringify(pos_res, null, 2))
+            console.log(JSON.stringify(pos_res, null, 2))
             position = pos_res.images[0].classifiers[0].classes[0].class;
             console.log(position);
             err = pos_res.images[0].error[0].description
             console.log(err);
-      //      fs.unlink('dog.jpg')
+            //fs.unlink('dog.png')
+          Webcam.clear();
         }
     });
 }

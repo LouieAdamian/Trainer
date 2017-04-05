5ca5de7ec9d6ef9930ef1ee2d135536b24ec10ea
Louie Adamian <LouieAdamian@users.noreply.github.com>
2017-04-05 10:10:27 -0400

    removes unnecessary files

--- app.js
+++ app.js
@@ -1,43 +1,64 @@
-const fs = require('fs');
-const exec = require('child_process').exec;
-const watson = require('watson-developer-cloud');
-const visual_recognition = watson.visual_recognition({
+var captured
+var position
+var NodeWebcam = require("node-webcam");
+var fs = require('fs');
+var watson = require('watson-developer-cloud');
+var visual_recognition = watson.visual_recognition({
     api_key: '290ef006ab8edf8c61fd8bed56fb9c85a103295a',
     version: 'v3',
     version_date: '2016-05-20'
 });
-takePhoto();
+var pos_res;
+var opts = {
+    width: [Number, 1920],
+    height: [Number, 1080],
+    delay: [Number, 0],
+
+    quality: [Number, 100],
+
+    output: [String, "jpg"],
+
+    verbose: [Boolean, true],
+
+    help: [Boolean, false],
+
+    version: [Boolean, false],
+    location: "./"
+
+}
+var img = {
+    images_file: fs.createReadStream('./dog.png'),
+    classifier_ids: 'position_185473635'
+}
+var Webcam = NodeWebcam.create(opts);
+Image_cap()
+
+function Image_cap() {
+    console.log("capture");
+    NodeWebcam.capture("dog.jpg", {}, function(err, data) {
+
+        if (!err) {
+            console.log("Image created!");
+
+        } else {
+            //console.log(err);
+        }
+        Webcam.clear()
+        classify();
+    });;
+}
 
 function classify() {
-
-    var img = {
-        images_file: fs.createReadStream('./dog2.jpg'),
-        classifier_ids: 'position_185473635'
-    }
+    console.log("classify");
     visual_recognition.classify(img, function(err, res) {
         if (err) {
             console.log(err);
         } else {
             pos_res = res;
-            console.log(JSON.stringify(pos_res, null, 2))
+            console.log(JSON.stringify(res));
             position = pos_res.images[0].classifiers[0].classes[0].class;
             console.log(position);
-            err = pos_res.images[0].error[0].description
-            console.log(err);
-            //fs.unlink('dog.png')
+
         }
     });
 }
-
-
-function takePhoto() {
-
-    exec("sudo fswebcam --no-banner fs-r 640*480 dog.jpg", (error, stdout, stderr) => {
-          if (error) {
-            console.error(`exec error: ${error}`);
-            return;
-        }
-        console.log(`stdout: ${stdout}`);
-        console.log(`stderr: ${stderr}`);
-        classify();
-    });
